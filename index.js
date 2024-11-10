40660b0210d4e0301ad814153a1c0a11d82b0f81
scynn24 <66961831+scynn24@users.noreply.github.com>
2024-11-10 19:33:20 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-M.Sidenav.init(document.querySelectorAll('.sidenav'), {edge: 'right'});  
+//M.Sidenav.init(document.querySelectorAll('.sidenav'), {edge: 'right'});  
+document.addEventListener('DOMContentLoaded', function() { 
+    // Initialize side navigation 
+    var sidenavElems = document.querySelectorAll('.sidenav'); M.Sidenav.init(sidenavElems, {edge: 'right'}); 
+    // You can add more Materialize component initializations here if needed 
+    });
